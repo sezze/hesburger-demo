2991dd3a53348a283fedc36e512d605e26db889b
sezze <9763776+sezze@users.noreply.github.com>
2023-04-27 09:32:07 +0300

    Update base-address.ts

--- utils/server/base-address.ts
+++ utils/server/base-address.ts
@@ -1,4 +1,8 @@
-const baseAddress =
+let baseAddress =
   process.env.VERCEL_URL ?? process.env.BASE_ADDRESS ?? "http://localhost:3000";
+
+if (!baseAddress.startsWith("http")) {
+  baseAddress = `https://${baseAddress}`;
+}
 
 export default baseAddress;
